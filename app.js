5e3356548d4b941f5313c1f12e11fe9aee3066c3
Matias-Budani <matias.budani@gm2dev.com>
2025-08-26 12:32:07 -0300

    feat(app.js): add exercise 1

---
+++ app.js
@@ -0,0 +1,6 @@
+// 1 Crea un bloque de código que compare dos variables numéricas. Si el número A es mayor que el número B, imprimir en consola ‘El número A es mayor a B‘.
+let numA = 4
+let numB = 2
+if(numA>numB){
+    console.log(numA + " es mayor que " + numB)
+}
