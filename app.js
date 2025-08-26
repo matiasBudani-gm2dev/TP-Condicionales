d812d02d8ca5e08b505fd434a6bc5366423fbfc7
Matias-Budani <matias.budani@gm2dev.com>
2025-08-26 12:58:20 -0300

    feat(app.js): add exercise 2

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const prompt = require("prompt-sync")()
 // 1 Crea un bloque de código que compare dos variables numéricas. Si el número A es mayor que el número B, imprimir en consola ‘El número A es mayor a B‘.
 let numA = 4
 let numB = 2
 if(numA>numB){
     console.log(numA + " es mayor que " + numB)
+}
+// 2 Crea un bloque de código que tome la edad de un usuario y la compare con un número de referencia. Si la edad es mayor al número de referencia, imprimir en consola un mensaje.
+let edad
+let numRef = 17
+do{
+    edad = prompt("Ingrese su edad")
+    if(edad % 1 == 0){
+        edad = parseInt(edad)
+    }
+}while(isNaN(edad) || edad === edad.toString())
+if(edad>numRef){
+    console.log("Eres mayor de edad")
 }
